0ca1fb04b35fa7804b8bdc23f423d2c1b8863826
thankinson <t_hankinson@outlook.com>
2022-03-12 13:15:48 +0000

    updated movie crud

--- src/movie/movieControllers.js
+++ src/movie/movieControllers.js
@@ -12,10 +12,38 @@ exports.addMovie = async (req, res) => {
 
 exports.listMovie = async (req, res) =>{
     try {
-        const movies = await Movie.find({});
-        res.status(200).send({allMovie: movies})
+        if (req){
+            const movies = await Movie.findOne({[req.param.filterKey]: req.param.filterVal});
+            res.status(200).send({movie: movies});
+        } else {
+            const movies = await Movie.find({});
+            res.status(200).send({allMovie: movies});
+        }
     } catch (error) {
         console.log(error)
+        res.status(500).send({err: error.message});
+    }
+};
+
+exports.updateMovie = async (req, res) => {
+    try {
+        const movies = await Movie.updateOne(
+            { [req.param.filterKey]: req.param.filterVal },
+            { [req.param.updateKey]: req.param.updateVal }
+        );
+        res.status(200).send({update: movies});
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({err: error.message});
+    }
+};
+
+exports.deleteMovie = async (req, res) => {
+    try {
+        const remove = await Movie.deleteOne({[re.param.filterKey]: req.param.deleteValue});
+        res.status(200).send({movie: remove})
+    } catch (error) {
+        console.log(error);
         res.status(500).send({err: error.message});
     }
 }
