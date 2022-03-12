4be2e2a3aca343c506c84af5ed2e99b5155d2c52
thankinson <t_hankinson@outlook.com>
2022-03-12 18:28:13 +0000

    updated delete. working on update

--- src/movie/movieControllers.js
+++ src/movie/movieControllers.js
@@ -10,32 +10,45 @@ exports.addMovie = async (req, res) => {
     }
 };
 
-exports.listMovie = async (req, res) =>{
+exports.listMovie = async (req, res) => {
     try {
-        const movies = await Movie.findAll();
-        res.status(200).send({allMovie: movies});
+        if (req.body.title){
+            const listOne = await Movie.findOne({where: {title: req.body.title}});
+            console.log(listOne)
+            res.status(200).send({movie: listOne})
+        } else {
+            const movies = await Movie.findAll({});
+            res.status(200).send({allMovie: movies});
+        }
     } catch (error) {
-        console.log(error)
-        res.status(500).send({err: error.message});
+        console.log(error);
+        res.status(500).send({err: error.message})
     }
-};
+}
 
-exports.updateMovie = async (req, res) => {
-    try {
-        const movies = await Movie.updateOne(
-            { [req.param.filterKey]: req.param.filterVal },
-            { [req.param.updateKey]: req.param.updateVal }
-        );
-        res.status(200).send({update: movies});
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({err: error.message});
-    }
-};
+// exports.updateMovie = async (req, res) => {
+//     try {
+//         const findMovie = await Movie.findOne({where: {title: req.body.title}});
+//         if (findMovie){
+//             const movies = await Movie.update({where: { title: req.body.title }});
+//             res.status(200).send({update: movies});
+//         } else {
+//             res.status(500).send({err: error.message})
+//         }
+//         // const movies = await Movie.update(
+//         //     { title: req.body.title },
+//         //     {where:{ title: req.body.title }}
+//         // );
+//         // res.status(200).send({update: movies});
+//     } catch (error) {
+//         console.log(error)
+//         res.status(500).send({err: error.message});
+//     }
+// };
 
 exports.deleteMovie = async (req, res) => {
     try {
-        const remove = await Movie.deleteOne({[re.param.filterKey]: req.param.deleteValue});
+        const remove = await Movie.destroy({where: {title: req.body.title}});
         res.status(200).send({movie: remove})
     } catch (error) {
         console.log(error);
--- src/movie/movieRoutes.js
+++ src/movie/movieRoutes.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
-const { addMovie, listMovie} = require("./movieControllers");
+const { addMovie, listAllMovie, listMovie, deleteMovie, updateMovie} = require("./movieControllers");
 const movieRouter = Router();
 
 // endpoints
 movieRouter.post("/movie", addMovie);
+// movieRouter.get("/movie", listAllMovie);
 movieRouter.get("/movie", listMovie);
+movieRouter.delete("/movie", deleteMovie );
+// movieRouter.patch("/movie", updateMovie)
 
 module.exports = movieRouter;
